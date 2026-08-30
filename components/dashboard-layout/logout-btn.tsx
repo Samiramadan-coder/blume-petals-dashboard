3b05120a8e6a7c625e17f21ad7c473a18c4c80f3
sami-ramadan <samiramadan389@gmail.com>
2026-08-31 00:06:20 +0300

    test

--- components/dashboard-layout/logout-btn.tsx
+++ components/dashboard-layout/logout-btn.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { useRouter } from "@/i18n/navigation";
+import { deleteToken } from "@/lib/actions";
 import { User } from "@/types/shared";
 import { LogOut } from "lucide-react";
 
 export default function LogoutBtn({ user }: { user: User }) {
+  const router = useRouter();
+
   return (
-    <div className="cursor-pointer text-muted-foreground mt-auto p-4 border-t border-primary/30 text-xs flex items-center justify-center gap-4">
+    <div
+      onClick={async () => {
+        await deleteToken();
+        router.push("/login");
+      }}
+      className="cursor-pointer text-muted-foreground mt-auto p-4 border-t border-primary/30 text-xs flex items-center justify-center gap-4"
+    >
       <div className="h-8 w-8 rounded-full bg-primary grid place-items-center font-bold">
         {user.name.charAt(0).toUpperCase()}
       </div>
